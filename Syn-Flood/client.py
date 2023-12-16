f2ee2b0fee1eb0ec4cdb352c139b85498ec36755
and4r1lh0 <kdevops32@gmail.com>
2023-12-16 18:45:07 +0400

    update v.3.4.2

---
+++ Syn-Flood/client.py
@@ -0,0 +1,25 @@
+import socket
+import psutil
+import os
+
+
+def main():
+    while(1):
+        client_id = os.getenv("COMPUTERNAME")
+        processor_load = psutil.cpu_percent()
+        message = f"{client_id} {processor_load}"
+
+        # Пытаемся подключиться к серверу
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect(("localhost", 8000))
+            client_socket.send(message.encode())
+            data = client_socket.recv(1024).decode()
+            print(data)
+        except ConnectionRefusedError:
+            # Если сервер не доступен, выводим сообщение
+            print(f"Сервер не доступен.")
+
+
+if __name__ == "__main__":
+    main()
