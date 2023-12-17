e689483b8ca9f220c73976436b4762b185593d24
and4r1lh0 <kdevops32@gmail.com>
2023-12-17 15:34:18 +0400

    update v.3.6.1

--- provisioning/init.ps1
+++ provisioning/init.ps1
@@ -1,12 +1,12 @@
-Start-BitsTransfer -Source "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe" -Destination "$env:TEMP\python_installer.exe" | Out-Null
+Start-BitsTransfer -Source "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe" -Destination "$env:TEMP\python_installer.exe"
 Start-Process -FilePath "$env:TEMP\python_installer.exe" -ArgumentList "/quiet", "PrependPath=1" -Wait
-Remove-Item "$env:TEMP\python_installer.exe"
 
-Start-BitsTransfer -Source "https://npcap.com/dist/npcap-1.78.exe" -Destination "$env:TEMP\npcap.exe" | Out-Null
+Start-BitsTransfer -Source "https://npcap.com/dist/npcap-1.78.exe" -Destination "$env:TEMP\npcap.exe"
 Start-Process -FilePath "$env:TEMP\npcap.exe" -ArgumentList "/quiet", "PrependPath=1" -Wait
+
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/and4r1lh0/DDoS/master/provisioning/ICMP-Flood.zip" -Destination "./ICMP-Flood.zip"
+Expand-Archive -Path "./ICMP-Flood.zip" -Force
+
+Remove-Item "$env:TEMP\python_installer.exe"
 Remove-Item "$env:TEMP\npcap.exe"
-
-Start-BitsTransfer -Source "https://codeload.github.com/and4r1lh0/DDoS/zip/refs/heads/master" -Destination "./master.zip"
-
-Expand-Archive -Path "./master.zip" -DestinationPath "./DDoS" -Force
-Remove-Item "./master.zip"
+Remove-Item "./ICMP-Flood.zip"
